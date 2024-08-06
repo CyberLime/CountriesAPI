f3588bd5155d2d1ba09d09540330eaaa179b137a
CyberLime <togrul2006@inbox.ru>
2024-08-06 13:43:32 +0400

    spa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, build } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CountriesAPI/',
-})
+  base: "/CountriesAPI",
+  appType: "spa",
+});
